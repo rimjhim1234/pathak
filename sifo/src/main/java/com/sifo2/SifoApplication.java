2e2ddfe0e89ad1db22f02797df3a16d523dd2c83
aakriti <aakriti18899@gmail.com>
2023-04-05 10:09:04 +0530

    ct

---
+++ sifo/src/main/java/com/sifo2/SifoApplication.java
@@ -0,0 +1,13 @@
+package com.sifo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SifoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SifoApplication.class, args);
+	}
+
+}
---
+++ sifo/src/test/java/com/sifo2/SifoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sifo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SifoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
